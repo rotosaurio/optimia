1251a5f3ec66394b0bf952c99456cae398980d6f
Edgar Alfredo Rivera Díaz <edgarafedo123@gmail.com>
2023-11-28 14:19:46 -0600

    mejora en el chat

--- pages/chat.js
+++ pages/chat.js
@@ -34,32 +34,31 @@ export default function Home() {
   }
 
   return (
-  <div className="mx-auto p-4 bg-gray-100 rounded-md shadow-md">
-  <div className="border p-4 mb-4 bg-white rounded-md shadow-md">
-    {/* Historial del chat */}
-    {chatHistory.map((message, index) => (
-      <div key={index} className={message.role === 'user' ? 'text-right mb-4' : 'mb-3'}>
-        <span className={message.role === 'user' ? 'bg-blue-500 text-white p-3 rounded-md' : 'bg-gray-300 p-3 rounded-md block'}>
-          {message.content}
-        </span>
-      </div>
-    ))}
-  </div>
+  <div className="fixed bottom-0 left-0 right-0 mx-auto p- bg-gray-100 rounded-md shadow-md">
+    <div className="border p-4 mb-4 bg-white rounded-md shadow-md">
+      {/* Historial del chat */}
+      {chatHistory.map((message, index) => (
+        <div key={index} className={message.role === 'user' ? 'text-right mb-4' : 'mb-3'}>
+          <span className={message.role === 'user' ? 'bg-blue-500 text-white p-3 rounded-md' : 'bg-gray-300 p-3 rounded-md block'}>
+            {message.content}
+          </span>
+        </div>
+      ))}
+    </div>
 
-  {/* Campo de entrada y botón de enviar */}
-  <div className="flex">
-    <input
-      type="text"
-      value={userInput}
-      onChange={handleInputChange}
-      className="flex-grow border p-3 mr-2 rounded-md focus:outline-none"
-      placeholder="Escribe un mensaje..."
-    />
-    <button onClick={sendData} className="bg-blue-500 text-white p-3 rounded-md hover:bg-blue-600 focus:outline-none">
-      Enviar 
-    </button>
+    {/* Campo de entrada y botón de enviar */}
+    <div className="flex">
+      <input
+        type="text"
+        value={userInput}
+        onChange={handleInputChange}
+        className="flex-grow border p-3 mr-2 rounded-md focus:outline-none"
+        placeholder="Escribe un mensaje..."
+      />
+      <button onClick={sendData} className="bg-blue-500 text-white p-3 rounded-md hover:bg-blue-600 focus:outline-none">
+        Enviar 
+      </button>
+    </div>
   </div>
-</div>
-
   );
 }
